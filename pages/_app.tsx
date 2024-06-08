78c15ea8950aaeb8483f54bed544c3a695389d15
vitorreisler <vitorreisler@gmail.com>
2024-06-08 09:15:10 -0300

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '/styles/global.css'
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from '@material-tailwind/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
